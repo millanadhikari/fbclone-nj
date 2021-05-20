16451ae58e814622d031140a1f75b468cac2f1e9
millanadhikari <millanadhikari@gmail.com>
2021-05-20 18:23:49 +1000

    Header and next auth done

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import {getSession} from 'next-auth/client'
+import Login from '../components/Login'
 
-export default function Home() {
+
+export default function Home({session}) {
+  if(!session) return <Login/>
   return (
     <div className="flex">
       <Head>
-        <title>Create Next App</title>
+        <title>Facebook</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="">Facebook Clone</h1>
-      <h2 className="">lkjsdf</h2>
+      <Header/>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  return {
+    props:{
+      session
+    }
+  }
 }
